5ba66c901a9a20ba9730b9cb602b1a3a0618ca7d
jesuisMQ <quancun2004@gmail.com>
2024-01-25 18:25:54 +0700

    third commit

--- lab2/Bài trang 47/tinhtongcacso.java
+++ lab2/Bài trang 47/tinhtongcacso.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 class tinhtongcacso{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int a, sum=0;
+        int a, sum=0,i=1;
+        String str ="";
+       
         System.out.println("nhap cac so nguyen");
-        while(sum<100){   
-            System.out.println("a=");
+        while(sum<=100){   
+            System.out.println("a"+"["+i+"]"+"=");
+            i++;
             a = sc.nextInt();
             sum += a;
+            str=str+a+"+";
         }
-    
-
+        str = str.substring(0,(str.length()-2));
+        System.out.print(str + "="+sum);
     }
 }
